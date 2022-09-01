99ad9eca6a2bf2735c8d5fa5667514ad194b4a03
Tejas1045 <82049342+Tejas1045@users.noreply.github.com>
2022-09-01 18:53:57 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+region = terraform.workspace == "default" ? "us-east-2" : "us-west-1"
+}
+
+data "aws_ssm_parameter" "linuxAmi" {
+name="/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
+}
+
+resource "aws_instance" "ec2-vm" {
+ami= data.aws_ssm_parameter.linuxAmi.value
+instance_type ="t2.micro"
+associate_public_ip_address=true
+tags={
+Name= "${terraform.workspace}-ec2"
+}
+}
